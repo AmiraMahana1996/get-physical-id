eda50be4776fc2c804ce27cf35ecb2f1385d2e0c
Amira Saber (Developer) <Amira.Saber@elezabypharmacy.com>
2023-06-12 14:54:05 +0300

    init

---
+++ test.py
@@ -0,0 +1,19 @@
+import uuid
+import socket
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+
+
+def get_mac_address():
+    mac_address = uuid.UUID(int=uuid.getnode()).hex[-12:]
+    return ':'.join([mac_address[i:i+2] for i in range(0, len(mac_address), 2)])
+
+phyId=get_mac_address()
+@app.get("/")
+async def home():
+    return {"Physical Address":phyId }
